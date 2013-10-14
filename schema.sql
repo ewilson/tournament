05eb336bb3ba7b45fe87e47b5192ac0eff2caa12
Eric Wilson <wilson.eric.n@gmail.com>
2013-10-14 04:39:55 -0400

    Added description to tournament

--- schema.sql
+++ schema.sql
@@ -2,6 +2,7 @@ drop table if exists tournament;
 create table tournament (
        id integer primary key autoincrement,
        start_date text not null,
+       description text not null,
        tourn_type text not null
 );
 
