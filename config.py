1d36d8468d46168776e26c7d0aab10220241c7c8
Eric Wilson <wilson.eric.n@gmail.com>
2013-11-08 21:49:07 -0500

    Added requirements.txt

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 DATABASE = 'data/tourn.db'
-SCHEMA = 'data/schema.sql'
-TEST_DATABASE = 'data/tourn_test.db'
+SCHEMA = '../data/schema.sql'
+TEST_DATABASE = '../data/tourn_test.db'
 CSRF_ENABLED = True
 SECRET_KEY = 'super-secure-open-source-project'
--- run.py
+++ run.py
@@ -1,4 +1,2 @@
-#!/usr/bin/python
-
 from app import app
 app.run(debug=True)
