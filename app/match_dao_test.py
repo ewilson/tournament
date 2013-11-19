f18b73d7caa9567eaa2dbf6e6c181636a887dfdc
Eric Wilson <wilson.eric.n@gmail.com>
2013-11-19 05:23:01 -0500

    Added db fk constraints to integration tests

--- app/match_dao_test.py
+++ app/match_dao_test.py
@@ -1,21 +1,23 @@
 import pytest
 import sqlite3
 
-import match_dao, player_dao
+import match_dao, player_dao, tournament_dao
 import config
-from models import Player,Match
+from models import Player, Match, Tournament
 
 class FakeG(object):
     def __init__(self):
         self.db = sqlite3.connect(config.TEST_DATABASE)
         script = open(config.SCHEMA).read()
         self.db.executescript(script)
+        self.db.execute('pragma foreign_keys = ON')
 
 @pytest.fixture
 def g():
     fG = FakeG()
     match_dao.g = fG
     player_dao.g = fG
+    tournament_dao.g = fG
 
 def test_create_and_find_match(g):
     p = Player("test player")
@@ -24,14 +26,28 @@ def test_create_and_find_match(g):
     p.id = 1
     player_dao.create(p2)
     p2.id = 2
-    tournament_id = 3
+    t = Tournament(0,'','T1','type',0)
+    tournament_dao.create(t)
+    t.id = 1
 
-    match_dao.create([p.id, p2.id], tournament_id)
+    match_dao.create([p.id, p2.id], t.id)
     retrieved_match = match_dao.find(1)
 
     assert retrieved_match.id == 1
     assert retrieved_match.player1.fname == p.fname
     assert retrieved_match.player2.fname == p2.fname
 
-#def test_create_and_find_scheduled_by_tournament(g):
+foo = '''
+def test_create_and_find_scheduled_by_tournament(g):
+    p = Player("test player")
+    p2 = Player("test player 2")
+    p3 = Player("test player 3")
+    player_dao.create(p)
+    p.id = 1
+    player_dao.create(p2)
+    p2.id = 2
+    player_dao.create(p3)
+    p3.id = 3
+    match_dao
     
+'''
--- app/player_dao_test.py
+++ app/player_dao_test.py
@@ -10,6 +10,7 @@ class FakeG(object):
         self.db = sqlite3.connect(config.TEST_DATABASE)
         script = open(config.SCHEMA).read()
         self.db.executescript(script)
+        self.db.execute('pragma foreign_keys = ON')
 
 @pytest.fixture
 def g():
