7d24cb74cbe65beb235be3d251c307e5858afc11
Eric Wilson <wilson.eric.n@gmail.com>
2014-01-29 20:13:25 -0500

    Refactor started

--- app/static/js/player.js
+++ app/static/js/player.js
@@ -1,69 +1,42 @@
-var Players = function() {
-};
-Players.prototype.add = function(options) {
-    $.ajax({
-        url: $SCRIPT_ROOT + '/player',
-        type: 'POST',
-        dataType: 'json',
-        data: { fname: options.fname },
-        success: options.success
-    });
-};
-Players.prototype.delete = function(options) {
-    $.ajax({
-        url: $SCRIPT_ROOT + '/player',
-        type: 'DELETE',
-        dataType: 'json',
-        data: { id: options.id },
-        success: options.success
-    });
-};
+jQuery(function ($) {
+    'use strict';
 
-var NewPlayerView = function(options) {
-    this.players = options.players;
-    var add = $.proxy(this.addPlayer, this)
-    $('#new-player form').submit(add);
-};
-NewPlayerView.prototype.addPlayer = function(e) {
-    e.preventDefault();
+    var PlayerDao = {
+	add: function(options) {
+	    $.ajax({
+		url: $SCRIPT_ROOT + '/player',
+		type: 'POST',
+		dataType: 'json',
+		data: { fname: options.fname },
+		success: options.success
+	    });
+	},
+	remove: function(options) {
+	    $.ajax({
+		url: $SCRIPT_ROOT + '/player',
+		type: 'DELETE',
+		dataType: 'json',
+		data: { id: options.id },
+		success: options.success
+	    });
+	}
+    };
 
-    var that = this;
-
-    this.players.add({
-        fname: $('#fname').val(),
-        success: function(data) {
-	    that.appendPlayer(data);
-	    that.clearInput();
+    var App = {
+        init: function () {
+	    this.playerTemplate = Handlebars.compile($("#player-template").html());
+	    this.$newPlayerForm = $('#new-player form');
+            this.$players = $('#players');
+	    this.$fname = this.$newPlayerForm.find('#fname');
+	    this.$players.on('click','.del-link',this.removePlayer)
+        },
+	removePlayer: function(e) {
+	    e.preventDefault();
+	    var player_id = $(this).closest('.player-item').data('player_id');
+	    console.log('ID:',player_id)
+	    $(this).closest('.player-item').remove();
         }
-    });
-};
-NewPlayerView.prototype.appendPlayer = function(data) {
-    var playerTemplate = Handlebars.compile($("#player-template").html());
-    var playerData = {id: data.id, fname: data.fname}
-    var player = playerTemplate(playerData);
-    $('#players').append(player);
-};
-NewPlayerView.prototype.clearInput = function() {
-    $('#fname').val('');
-};
-
-
-$(document).ready(function() {
-    var players = new Players();
-    new NewPlayerView({ players: players });
-
-    $('#players').on('click', '.del-link', function(e) {
-	e.preventDefault();
-	player_id = $(this).closest('.player-item').data('player_id');
-        $.ajax({
-            url: $SCRIPT_ROOT + '/player/' + player_id,
-            type: 'DELETE',
-            dataType: 'json',  
-            success: function(data) {
-		var data_select = 'div[data-player_id=' + data.id + ']';
-		$('.player-item').filter(data_select).remove();
-            }
-        });
-    });
-
+    };
+    App.init();
 });
+
