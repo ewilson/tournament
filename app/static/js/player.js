e9932436aee306a133c74018bf654bfa1731f7af
Eric Wilson <wilson.eric.n@gmail.com>
2014-01-30 06:29:03 -0500

    Faking adding and deleting

--- app/static/js/player.js
+++ app/static/js/player.js
@@ -27,15 +27,25 @@ jQuery(function ($) {
 	    this.playerTemplate = Handlebars.compile($("#player-template").html());
 	    this.$newPlayerForm = $('#new-player form');
             this.$players = $('#players');
-	    this.$fname = this.$newPlayerForm.find('#fname');
-	    this.$players.on('click','.del-link',this.removePlayer)
+	    this.$fname = $('#fname');
+	    this.$players.on('click','.del-link',this.removePlayer);
+	    this.$newPlayerForm.submit(this.addPlayer);
         },
 	removePlayer: function(e) {
 	    e.preventDefault();
 	    var player_id = $(this).closest('.player-item').data('player_id');
-	    console.log('ID:',player_id)
 	    $(this).closest('.player-item').remove();
-        }
+        },
+	addPlayer: function(e) {
+	    e.preventDefault();
+	    var fname = App.$fname.val()
+	    App.appendPlayer({id:100, fname: fname});
+	    App.$fname.val('');
+        },
+	appendPlayer: function(data) {
+	    var player = App.playerTemplate(data);
+	    App.$players.append(player);
+	}
     };
     App.init();
 });
