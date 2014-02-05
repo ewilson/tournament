c58f6f93083c20f4bc77b0b6d7685b3bfabb0596
Eric Wilson <wilson.eric.n@gmail.com>
2014-02-05 05:36:25 -0500

    Tournament delete in progress -- not firing

--- app/static/js/create-tournament.js
+++ app/static/js/create-tournament.js
@@ -13,11 +13,10 @@ jQuery(function ($) {
 	},
 	remove: function(options) {
 	    $.ajax({
-		url: $SCRIPT_ROOT + '/player/' + options.player_id,
+		url: $SCRIPT_ROOT + '/tournament/' + options.tournament_id,
 		type: 'DELETE',
 		dataType: 'json',
-		success: options.success,
-		error: options.error
+		success: options.success
 	    });
 	},
 	list: function(options) {
@@ -38,6 +37,19 @@ jQuery(function ($) {
 	    this.$newTourneys = $('#New');
 	    this.$tournamentTemplate = Handlebars.compile($("#tournament-template").html());
 	    this.$newTournamentForm.submit(this.createTournament);
+	    this.$newTourneys.on('click','.del-link',this.deleteTourney);
+        },
+	deleteTourney: function(e) {
+	    e.preventDefault();
+	    var tournament = $(this).closest('.tourney-item');
+	    var tourney_id = player.data('tourney_id');
+	    TournamentDao.remove({
+		tournament_id: tournament_id,
+		success: function() {
+//		    tournament.remove();
+		    alert('deleting');
+		}
+	    });
         },
 	createTournament: function(e) {
 	    console.log('createTournament');
