09cbb099116c04f726d4940a30e9adacc916ba5d
Eric Wilson <wilson.eric.n@gmail.com>
2014-02-07 01:22:39 -0500

    Tourney lists display only when non-empty (home)

--- app/static/js/create-tournament.js
+++ app/static/js/create-tournament.js
@@ -41,6 +41,7 @@ jQuery(function ($) {
 	    this.$tournamentTemplate = Handlebars.compile($("#tournament-template").html());
 	    this.$newTournamentForm.submit(this.createTournament);
 	    this.$tourneys.on('click','.del-link',this.deleteTourney);
+	    this.$tourneys.hide();
 	    this.getNewTournaments();
 	    this.getActiveTournaments();
 	    this.getCompletedTournaments();
@@ -75,11 +76,15 @@ jQuery(function ($) {
 	    bootbox.confirm('Are you sure? There is no undelete!', function(value) {
 		if (value) {		
 		    var tournament = that.closest('.tourney-item');
+		    var tournament_list = that.closest('.tourneys');
 		    var tournament_id = tournament.data('tourney_id');
 		    TournamentDao.remove({
 			tournament_id: tournament_id,
 			success: function() {
 			    tournament.remove();
+			    if (!tournament_list.find('.tourney-item').length) {
+				tournament_list.hide();
+			    }
 			}
 		    });
 		}
@@ -102,11 +107,13 @@ jQuery(function ($) {
 	appendNewTournament: function(data) {
 	    var tourney = Page.$tournamentTemplate(data)
 	    Page.$newTourneys.append(tourney);
+	    Page.$newTourneys.show();
 	},
 	appendTournaments: function(data, div) {
 	    $.each(data.tournaments, function(i, tournament) {
 		var tourney = Page.$tournamentTemplate(tournament)
 		div.append(tourney);
+		div.show();
 	    });
 	}
     };
