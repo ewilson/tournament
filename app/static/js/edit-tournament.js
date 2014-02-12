14cb9ef7d5c36aa96ac0171ab48f3361f95a4fab
Eric Wilson <wilson.eric.n@gmail.com>
2014-02-12 05:01:42 -0500

    Retrieving player_ids to add to tourney

--- app/static/js/edit-tournament.js
+++ app/static/js/edit-tournament.js
@@ -6,9 +6,10 @@ jQuery(function ($) {
 	    this.$page = $('.container');
 	    this.$playerList = $('.player-list');
 	    this.$addPlayerTemplate = Handlebars.compile($("#add-player-template").html());
-	    this.$addForm = $('.form');
+	    this.$addForm = $('form');
 	    this.$playerList.on('click','.list-group-item',this.togglePlayer);
 	    this.getPlayers();
+	    this.$addForm.submit(this.addPlayers);
         },
 	getPlayers: function() {
 	    Dao.Player.list({
@@ -26,6 +27,16 @@ jQuery(function ($) {
 	togglePlayer: function(e) {
 	    e.preventDefault();
 	    $(this).toggleClass('active');
+	},
+	addPlayers: function(e) {
+	    e.preventDefault();
+	    var players = $('.active')
+	    var ids = players.map(function() {
+		return $(this).data("player_id");
+	    });
+	    console.log(JSON.stringify(ids.get()));
+	    var id = $('h1').data('tournament_id');
+	    console.log(id);
 	}
     };
     Page.init();
