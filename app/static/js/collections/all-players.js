ba8d1a47618c8503e8dcbe338ccb6bbfa84da78a
Eric Wilson <wilson.eric.n@gmail.com>
2014-07-05 11:48:03 -0400

    basic fetching works

--- app/static/js/collections/all-players.js
+++ app/static/js/collections/all-players.js
@@ -1,7 +1,15 @@
 var AllPlayers = Backbone.Collection.extend({
 
-    model: app.Player
+    model: Player,
+    url: '/api/player'
 
 });
 
 var allPlayers = new AllPlayers();
+allPlayers.fetch({success: function(collection, response, options) {
+    console.log('collection', collection);
+    console.log('response', response.players);
+    console.log('options', options);
+}, error: function() {
+    console.log('oops')
+}});
