0fb27088a532a428bd8e2a7ad32b4f0a498af100
Eric Wilson <wilson.eric.n@gmail.com>
2014-08-01 05:49:05 -0400

    navigation --in progress

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,11 +1,25 @@
 App = Ember.Application.create();
 
 App.Router.map(function() {
-  // put your routes here
+    this.route("home");
+    this.route("home", {path: "/"});
+    this.route("players", { path: "/players" });
 });
 
 App.IndexRoute = Ember.Route.extend({
   model: function() {
     return ['red', 'yellow', 'blue'];
   }
+});
+
+App.HomeRoute = Ember.Route.extend({
+    model: function() {
+        return ['red', 'yellow', 'black'];
+    }
+});
+
+App.PlayersRoute = Ember.Route.extend({
+    model: function() {
+        return ['green', 'yellow', 'blue'];
+    }
 });
