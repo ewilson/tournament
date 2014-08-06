8354c1632f7798eb14a1ae89d880d27d1bbac7d2
Eric Wilson <wilson.eric.n@gmail.com>
2014-08-06 05:20:39 -0400

    adding player works

---
+++ app/static/js/controllers/players.js
@@ -0,0 +1,16 @@
+App.PlayersController = Ember.ArrayController.extend({
+    actions: {
+        createPlayer: function() {
+            var fname = this.get('newFname');
+            if (!fname || !fname.trim()) { return false; }
+
+            var player = this.store.createRecord('player', {
+                fname: fname
+            });
+
+            this.set('newFname', '');
+            console.log(player);
+            player.save();
+        }
+    }
+});
