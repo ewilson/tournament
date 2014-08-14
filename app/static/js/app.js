22dfa6b7cee1ee736d0977bf1705b5d2c3e5690a
Eric Wilson <wilson.eric.n@gmail.com>
2014-08-14 05:14:05 -0400

    some tournament wiring

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,9 +1,12 @@
 App = Ember.Application.create();
 
 App.Router.map(function() {
-    this.route("tournaments");
-    this.route("tournaments", {path: "/"});
-    this.route("players", { path: "/players" });
+//    this.route("tournaments");
+//    this.route("tournaments", {path: "/"});
+    this.resource("players");
+    this.resource('tournaments', function() {
+        this.resource('tournament', { path: ':tournament_id'});
+    });
 });
 
 App.IndexRoute = Ember.Route.extend({
