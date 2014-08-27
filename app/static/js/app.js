060f762d32a390c5594e611279c5e681df45da41
Eric Wilson <wilson.eric.n@gmail.com>
2014-08-27 04:40:49 -0400

    ember routing static content

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,28 +1,6 @@
 App = Ember.Application.create();
 
 App.Router.map(function() {
-//    this.route("tournaments");
-//    this.route("tournaments", {path: "/"});
-    this.resource("players");
-    this.resource('tournaments', function() {
-        this.resource('tournament', { path: ':tournament_id'});
-    });
+    this.resource("tournaments", {path: "/"});
 });
 
-App.IndexRoute = Ember.Route.extend({
-  model: function() {
-      return this.store.find('tournament');
-  }
-});
-
-App.TournamentsRoute = Ember.Route.extend({
-    model: function() {
-        return this.store.find('tournament');
-    }
-});
-
-App.PlayersRoute = Ember.Route.extend({
-    model: function() {
-        return this.store.find('player');
-    }
-});
---
+++ app/static/js/models/tournaments.js
@@ -0,0 +1,3 @@
+/**
+ * Created by eric on 8/26/14.
+ */
